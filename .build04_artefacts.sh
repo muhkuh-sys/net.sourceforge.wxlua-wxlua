c9b865a69fef79741f3ca444ad094020edf11b58
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-08-28 18:02:49 +0200

    Build the artifacts.

---
+++ .build04_artefacts.sh
@@ -0,0 +1,103 @@
+#! /bin/bash
+set -e
+
+#-----------------------------------------------------------------------------
+#
+# Build all artefacts.
+#
+pushd build
+rm -rf net.sourceforge.wxlua-lua5.1-wxlua
+rm -rf net.sourceforge.wxlua-lua5.2-wxlua
+rm -rf net.sourceforge.wxlua-lua5.3-wxlua
+rm -rf lua5.1
+rm -rf lua5.2
+rm -rf lua5.3
+
+mkdir net.sourceforge.wxlua-lua5.1-wxlua
+mkdir net.sourceforge.wxlua-lua5.2-wxlua
+mkdir net.sourceforge.wxlua-lua5.3-wxlua
+mkdir -p lua5.1/windows_x86
+mkdir -p lua5.1/windows_x86_64
+mkdir -p lua5.1/ubuntu_1404_x86
+mkdir -p lua5.1/ubuntu_1404_x86_64
+mkdir -p lua5.1/ubuntu_1604_x86
+mkdir -p lua5.1/ubuntu_1604_x86_64
+mkdir -p lua5.1/ubuntu_1704_x86
+mkdir -p lua5.1/ubuntu_1704_x86_64
+mkdir -p lua5.2/windows_x86
+mkdir -p lua5.2/windows_x86_64
+mkdir -p lua5.2/ubuntu_1404_x86
+mkdir -p lua5.2/ubuntu_1404_x86_64
+mkdir -p lua5.2/ubuntu_1604_x86
+mkdir -p lua5.2/ubuntu_1604_x86_64
+mkdir -p lua5.2/ubuntu_1704_x86
+mkdir -p lua5.2/ubuntu_1704_x86_64
+mkdir -p lua5.3/windows_x86
+mkdir -p lua5.3/windows_x86_64
+mkdir -p lua5.3/ubuntu_1404_x86
+mkdir -p lua5.3/ubuntu_1404_x86_64
+mkdir -p lua5.3/ubuntu_1604_x86
+mkdir -p lua5.3/ubuntu_1604_x86_64
+mkdir -p lua5.3/ubuntu_1704_x86
+mkdir -p lua5.3/ubuntu_1704_x86_64
+
+
+
+tar --extract --directory lua5.1/windows_x86 --file build_windows_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/windows_x86 --file build_windows_x86_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/windows_x86 --file build_windows_x86_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/windows_x86_64 --file build_windows_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/windows_x86_64 --file build_windows_x86_64_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/windows_x86_64 --file build_windows_x86_64_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1404_x86 --file build_ubuntu_1404_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1404_x86 --file build_ubuntu_1404_x86_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1404_x86 --file build_ubuntu_1404_x86_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1404_x86_64 --file build_ubuntu_1404_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1404_x86_64 --file build_ubuntu_1404_x86_64_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1404_x86_64 --file build_ubuntu_1404_x86_64_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1604_x86 --file build_ubuntu_1604_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1604_x86 --file build_ubuntu_1604_x86_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1604_x86 --file build_ubuntu_1604_x86_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1604_x86_64 --file build_ubuntu_1604_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1604_x86_64 --file build_ubuntu_1604_x86_64_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1604_x86_64 --file build_ubuntu_1604_x86_64_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1704_x86 --file build_ubuntu_1704_x86_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1704_x86 --file build_ubuntu_1704_x86_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1704_x86 --file build_ubuntu_1704_x86_lua5.3.tar.gz --gzip
+
+tar --extract --directory lua5.1/ubuntu_1704_x86_64 --file build_ubuntu_1704_x86_64_lua5.1.tar.gz --gzip
+tar --extract --directory lua5.2/ubuntu_1704_x86_64 --file build_ubuntu_1704_x86_64_lua5.2.tar.gz --gzip
+tar --extract --directory lua5.3/ubuntu_1704_x86_64 --file build_ubuntu_1704_x86_64_lua5.3.tar.gz --gzip
+
+popd
+
+
+pushd build/net.sourceforge.wxlua-lua5.1-wxlua
+cmake -DCMAKE_INSTALL_PREFIX="" ../../wxlua/installer/lua5.1
+make
+make package
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/net/sourceforge/wxlua/wxlua/*/lua5.1-wxlua-*.xml
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/net/sourceforge/wxlua/wxlua/*/lua5.1-wxlua-*.tar.xz
+popd
+
+pushd build/net.sourceforge.wxlua-lua5.2-wxlua
+cmake -DCMAKE_INSTALL_PREFIX="" ../../wxlua/installer/lua5.2
+make
+make package
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/net/sourceforge/wxlua/wxlua/*/lua5.2-wxlua-*.xml
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/net/sourceforge/wxlua/wxlua/*/lua5.2-wxlua-*.tar.xz
+popd
+
+pushd build/net.sourceforge.wxlua-lua5.1-wxlua
+cmake -DCMAKE_INSTALL_PREFIX="" ../../wxlua/installer/lua5.3
+make
+make package
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/net/sourceforge/wxlua/wxlua/*/lua5.3-wxlua-*.xml
+python2.7 ../../cmake/tools/generate_hash.py targets/jonchki/repository/net/sourceforge/wxlua/wxlua/*/lua5.3-wxlua-*.tar.xz
+popd
